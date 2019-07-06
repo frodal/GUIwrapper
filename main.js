bded9fb4876b5a8f19ac4622c57ce84f0bfd6f46
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-07-06 13:34:49 +0200

    :closed_book: Added terminate program button

--- main.js
+++ main.js
@@ -5,15 +5,18 @@ const {app, BrowserWindow} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow () 
+{
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow(
+    {
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    webPreferences: 
+        {
+            nodeIntegration: true
+        }
+    })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -22,7 +25,8 @@ function createWindow () {
   //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function () 
+  {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -36,18 +40,22 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function () 
+{
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') 
+  {
     app.quit()
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function () 
+{
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (mainWindow === null) 
+  {
     createWindow()
   }
 })
@@ -76,6 +84,11 @@ ipcMain.on('open-error-dialog', (event)=>
 {
     dialog.showErrorBox('Error', 'Could not execute the selected program!\nPlease select a valid executable program')
 })
+// Opens an error dialog message
+ipcMain.on('open-errorEXE-dialog', (event)=>
+{
+    dialog.showErrorBox('Error', 'Could not execute the selected program!')
+})
 
 // Opens a warning dialog message
 ipcMain.on('open-warning-dialog', (event)=>
@@ -97,7 +110,20 @@ ipcMain.on('open-isRunning-dialog', (event)=>
     {
         type:"info",
         title:"Warning",
-        message:"Please close the running application before starting a new one",
+        message:"Please terminate the running application before starting a new one",
+        buttons:['Ok']
+    }
+    dialog.showMessageBox(options)
+})
+
+// Opens a success dialog message
+ipcMain.on('open-successfulTermination-dialog', (event)=>
+{
+    const options = 
+    {
+        type:"info",
+        title:"Success",
+        message:"Application terminated successfully",
         buttons:['Ok']
     }
     dialog.showMessageBox(options)
--- renderer.js
+++ renderer.js
@@ -5,58 +5,91 @@
 const {ipcRenderer} = require('electron')
 const {execFile} = require('child_process')
 
+const selectProgramBtn = document.getElementById('SelectProgramBtn')
 const startProgramBtn = document.getElementById('StartProgramBtn')
+const terminateProgramBtn = document.getElementById('TerminateProgramBtn')
 let exePath = ''
 let exeCommandArgs = [''];
-let isRunning = false
+let subProcess = null
 
-startProgramBtn.addEventListener('click', (event)=>
-{
-    console.log('Start Button clicked!')
-    if(exePath!=='')
-    {
-        if(isRunning)
-        {
-            ipcRenderer.send('open-isRunning-dialog')
-        }else
-        {
-            document.getElementById('OutputData').innerHTML = ''
-            isRunning = true
-            try
-            {
-                execFile(exePath, exeCommandArgs, function(err, data)
-                {
-                    isRunning = false
-                    console.log(err)
-                    console.log(data.toString())
-                    // sets the output data and replacing \n with <br/> performs a global replacement with /\n/g
-                    document.getElementById('OutputData').innerHTML = `${data.toString().replace(/\n/g,'<br/>')}`
-                })
-            }
-            catch(err)
-            {
-                isRunning = false
-                console.log(err)
-                ipcRenderer.send('open-error-dialog')
-                document.getElementById('OutputData').innerHTML = `${err.toString().replace(/\n/g,'<br/>')} <br/>`
-            }
-        }
-    }else
-    {
-        console.log('Please select an executable file first!')
-        ipcRenderer.send('open-warning-dialog')
-    }
-})
-
-const selectProgramBtn = document.getElementById('SelectProgramBtn')
-
+////////////////////////////////////////////////////////////////////////////////////
+//                               Select Program                                   //
+////////////////////////////////////////////////////////////////////////////////////
+// Sets select program button callback
 selectProgramBtn.addEventListener('click', (event)=>
 {
     console.log('Select Button clicked!')
     ipcRenderer.send('open-file-dialog')
 })
+// Sets the executable filepath received from the main process (main.js)
 ipcRenderer.on('SelectedFile', (event, path)=>
 {
     document.getElementById('FilePath').innerHTML = `${path.toString()}`
     exePath = path.toString()
+})
+
+////////////////////////////////////////////////////////////////////////////////////
+//                                Start Program                                   //
+////////////////////////////////////////////////////////////////////////////////////
+// Sets start program button callback
+startProgramBtn.addEventListener('click', (event)=>
+{
+    console.log('Start Button clicked!')
+    if(exePath!=='')
+    {
+        if(subProcess!==null) // Check if a subprocess is already running
+        {
+            ipcRenderer.send('open-isRunning-dialog')
+        }else
+        {
+            // Clear output data field
+            document.getElementById('OutputData').innerHTML = ''
+            try // Try to execute the program and sets a callback for when the program terminates
+            {
+                subProcess = execFile(exePath, exeCommandArgs, function(err, data)
+                {
+                    if(err!==null && !subProcess.killed)
+                    {
+                        ipcRenderer.send('open-errorEXE-dialog')
+                    }else
+                    {
+                        ipcRenderer.send('open-successfulTermination-dialog')
+                    }
+                    console.log(err)
+                    console.log(data.toString())
+                    // sets the output data and replacing \n with <br/> performs a global replacement with /\n/g
+                    document.getElementById('OutputData').innerHTML = `${data.toString().replace(/\n/g,'<br/>')}`
+                    subProcess = null
+                })
+            }
+            catch(err) // Catches the error if the file selected can't be executed correctly
+            {
+                subProcess = null
+                console.log(err)
+                ipcRenderer.send('open-error-dialog')
+                // sets the output data and replacing \n with <br/> performs a global replacement with /\n/g
+                document.getElementById('OutputData').innerHTML = `${err.toString().replace(/\n/g,'<br/>')} <br/>`
+            }
+        }
+    }else
+    {
+        // Sends a warning no file path is selected
+        console.log('Please select an executable file first!')
+        ipcRenderer.send('open-warning-dialog')
+    }
+})
+
+////////////////////////////////////////////////////////////////////////////////////
+//                               Terminate Program                                //
+////////////////////////////////////////////////////////////////////////////////////
+// Sets terminate program button callback
+terminateProgramBtn.addEventListener('click', (event)=>
+{
+    if(subProcess!==null)
+    {
+        subProcess.kill()
+    }else
+    {
+        ipcRenderer.send('open-successfulTermination-dialog')
+    }
 })
