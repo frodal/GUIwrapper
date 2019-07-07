9846a342a406c1649656528fe5c2b1e624803f53
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-07-07 12:32:19 +0200

    Update frameless window

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () 
 {
@@ -17,10 +17,10 @@ function createWindow ()
         {
             nodeIntegration: true
         }
-    })
+    });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
@@ -31,14 +31,14 @@ function createWindow ()
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () 
@@ -47,9 +47,9 @@ app.on('window-all-closed', function ()
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') 
   {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () 
 {
@@ -57,19 +57,14 @@ app.on('activate', function ()
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) 
   {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-const {ipcMain, dialog} = require('electron')
-
-ipcMain.on('close-app', (event)=>
-{
-    mainWindow.close()
-})
+const {ipcMain, dialog} = require('electron');
 
 // Open file dialog to open file
 ipcMain.on('open-file-dialog', (event)=>
@@ -80,21 +75,21 @@ ipcMain.on('open-file-dialog', (event)=>
   {
       if(files)
       {
-          event.sender.send('SelectedFile', files)
+          event.sender.send('SelectedFile', files);
       }
-  })
-})
+  });
+});
 
 // Opens an error dialog message
 ipcMain.on('open-error-dialog', (event)=>
 {
-    dialog.showErrorBox('Error', 'Could not execute the selected program!\nPlease select a valid executable program')
-})
+    dialog.showErrorBox('Error', 'Could not execute the selected program!\nPlease select a valid executable program');
+});
 // Opens an error dialog message
 ipcMain.on('open-errorEXE-dialog', (event)=>
 {
-    dialog.showErrorBox('Error', 'Could not execute the selected program!')
-})
+    dialog.showErrorBox('Error', 'Could not execute the selected program!');
+});
 
 // Opens a warning dialog message
 ipcMain.on('open-warning-dialog', (event)=>
@@ -105,9 +100,9 @@ ipcMain.on('open-warning-dialog', (event)=>
         title:"Warning",
         message:"Please select an executable file first!",
         buttons:['Ok']
-    }
-    dialog.showMessageBox(options)
-})
+    };
+    dialog.showMessageBox(options);
+});
 
 // Opens a warning dialog message
 ipcMain.on('open-isRunning-dialog', (event)=>
@@ -118,8 +113,8 @@ ipcMain.on('open-isRunning-dialog', (event)=>
         title:"Warning",
         message:"Please terminate the running application before starting a new one",
         buttons:['Ok']
-    }
-    dialog.showMessageBox(options)
+    };
+    dialog.showMessageBox(options);
 })
 
 // Opens a success dialog message
@@ -131,6 +126,6 @@ ipcMain.on('open-successfulTermination-dialog', (event)=>
         title:"Success",
         message:"Application terminated successfully",
         buttons:['Ok']
-    }
-    dialog.showMessageBox(options)
+    };
+    dialog.showMessageBox(options);
 })
