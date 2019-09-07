14386face1d77f7023a9ea340a94655a19c6e2f5
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-09-07 14:06:20 +0200

    Now displays the License in the app from the dropdown menu

--- src/javascript/mainmenu.js
+++ src/javascript/mainmenu.js
@@ -1,5 +1,25 @@
 const {app, Menu, shell, dialog, nativeImage, BrowserWindow} = require('electron');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+
+const appName = app.getName();
+const appIcon = nativeImage.createFromPath(path.join(__dirname,'../../assets/icons/png/64x64.png'))
+let licenseString = '';
+
+function GetLicense()
+{
+    if(licenseString==='')
+    {
+        try
+        {
+            licenseString = fs.readFileSync(path.join(__dirname,'../../LICENSE.md'));
+        }catch(err)
+        {
+            licenseString = 'Copyright (c) 2019 Bjørn Håkon Frodal';
+        }
+    }
+    return licenseString;
+};
 
 const template = [
   {
@@ -34,7 +54,17 @@ const template = [
       },
       {
         label: 'View License',
-        click () { shell.openExternal('https://github.com/frodal/GUIwrapper/blob/master/LICENSE.md') }
+        click () {
+            GetLicense();
+            dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
+                type:"info",
+                title: 'License',
+                message: appName+" License\n\n"+
+                        licenseString,
+                buttons:['Ok'],
+                icon: appIcon
+            });
+        }
       },
       {
         type: 'separator'
@@ -58,15 +88,15 @@ const template = [
         click () {
             dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
                 type:"info",
-                title: app.getName(),
-                message: app.getName()+"\n"+
+                title: 'About '+appName,
+                message: appName +"\n"+
                         "Version: "+app.getVersion()+"\n\n"+
                         "Built with \n"+
                         "Electron: "+process.versions.electron+"\n"+
                         "Chrome: "+process.versions.chrome+"\n"+
                         "Node.js: "+process.versions.node,
                 buttons:['Ok'],
-                icon: nativeImage.createFromPath(path.join(__dirname,'../../assets/icons/png/64x64.png'))
+                icon: appIcon
             });
         }
       }
